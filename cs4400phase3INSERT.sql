2621f23d5857818f741ab7e123e46ffee9f46855
Aadarsh Battula <aadarshb123@gmail.com>
2024-07-22 13:44:08 +0200

    changed some stuff with insert and adde dmore data

--- cs4400phase3INSERT.sql
+++ cs4400phase3INSERT.sql
@@ -7,28 +7,41 @@
 
 insert into airline values
 ('Delta', 200),
-('RyanAir', 1);
+('RyanAir', 1),
+('Emirates', 33),
+('Vueling', 994);
 
 insert into location values
-('ATL'),
-('NY');
+('locationid1'),
+('locationid2'),
+('locationid3'),
+('locationid4');
 
 insert into airplane values
-('Delta', '2390847', 40, 400, 'ATL', 'jet', 0, 0, 4),
-('RyanAir', '210394', 30, 300, 'ATL', 'prop', 1, 4, 0);
+('Delta', '2390847', 40, 400, 'locationid1', 'jet', 0, 0, 4),
+('RyanAir', '210394', 30, 300, 'locationid2', 'prop', 1, 4, 0),
+('Emirates', '283838', 30, 200, 'locationid3', 'jet', 0, 0, 2),
+('Vueling', '338383', 20, 490, 'locationid4', 'prop', 1, 2, 0);
 
 insert into airport values
-('A2C', 'Hartsfield-Jackson', 'Atlanta', 'Georgia', 'USA', 'ATL'),
-('B3D', 'JFK', 'Queens', 'New York', 'USA', 'NY');
+('ATL', 'Hartsfield-Jackson', 'Atlanta', 'Georgia', 'USA', 'locationid1'),
+('NYC', 'John F. Kennedy', 'Queens', 'New York', 'USA', 'locationid2'),
+('DXB', 'Dubai International', 'Dubai', 'SomeProvince', 'UAE', 'locationid3'),
+('BCN', 'Llobregat El Prat', 'Barcelona', 'Catalonia', 'ESP', 'locationid4');
 
 insert into leg values
-('Leg1ID', 402, 'A2C', 'B3D');
+('Leg1ID', 402, 'ATL', 'NYC'),
+('Leg2ID', 300, 'DXB', 'BCN');
 
 insert into route values
-('Route1ID');
+('Route1ID'),
+('Route2ID');
 
 insert into route_path values
-('Route1ID', 'Leg1ID', 3);
+('Route1ID', 'Leg1ID', 3),
+('Route2ID', 'Leg2ID', 4);
 
 insert into flight values
-('Flight1ID', 'Route1ID', 'Delta', '2390847', 3, 'on_ground', '2024-07-21 12:30:34', 2000); 
+('Flight1ID', 'Route1ID', 'Delta', '2390847', 3, 'on_ground', '2024-07-21 12:30:34', 2000),
+('Flight2ID', 'Route2ID', 'Emirates', '283838', 2, 'in_flight', '2024-07-21 11:30:00', 30300); 
+
